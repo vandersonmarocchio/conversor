f295fe1c5971ec2a843ae880aa48e2df1223e2a9
Vanderson Marocchio <vanderson@sistemas.com.br>
2020-09-25 00:35:50 -0300

    Add segmentos

---
+++ segmentos/src/UUIDUtil.java
@@ -0,0 +1,65 @@
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
+public class UUIDUtil {
+
+    private static String macAddress;
+    private static long lastLocalId;
+
+    protected UUIDUtil() {
+
+    }
+
+    /**
+     * Gera UUID
+     * @return UUID
+     */
+    public static synchronized String generate() {
+        long localId = System.currentTimeMillis()*10000;
+
+        while (localId <= lastLocalId) {
+            localId++;
+        }
+
+        lastLocalId = localId;
+
+        return "" + Long.toHexString(localId).toUpperCase() + String.format("%06X", ((long)( Math.random() * 0x1000000))) + getMacValue();
+    }
+
+    /**
+     * Identifica endereço de MAC
+     * @return MAC
+     */
+    private static String getMacValue() {
+        if(macAddress == null) {
+            String toReturn = "";
+            try {
+                Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+                while (networkInterfaces.hasMoreElements()) {
+                    NetworkInterface network = networkInterfaces.nextElement();
+                    byte[] mac = network.getHardwareAddress();
+                    if (mac == null) {
+                        continue;
+                    }
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < mac.length; i++) {
+                        sb.append(String.format("%02X", mac[i]));
+                    }
+                    toReturn += sb.toString();
+                    break;
+                }
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            if (toReturn.length() == 0) {
+                macAddress = "6D756E696666";
+                return macAddress;
+            }
+            macAddress = toReturn;
+            return macAddress;
+        }
+
+        return macAddress;
+    }
+}
